b69f734c6239daa451edc0313c50ccdc4964e106
Alexander Panfilenok <alexander_panfilenok@epam.com>
2024-03-23 18:10:06 +0100

    remove extra code from addcol.py

--- Libraries/python/dabdemo/dabdemo/addcol.py
+++ Libraries/python/dabdemo/dabdemo/addcol.py
@@ -1,16 +1,4 @@
 import pyspark.sql.functions as F
-import logging
-
-logging.basicConfig(format='%(asctime)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    level=logging.INFO,
-                    )
-
-def start():
-  logging.info("Starting processing job.")
 
 def with_status(df):
   return df.withColumn("status", F.lit("checked"))
-
-if __name__ == "__main__":
-    start()
