da07c5655dc0696f7833d1af373386fd67bdabe9
Alexander Panfilenok <alexander_panfilenok@epam.com>
2024-03-23 13:30:41 +0100

    configure logger

--- Libraries/python/dabdemo/dabdemo/addcol.py
+++ Libraries/python/dabdemo/dabdemo/addcol.py
@@ -1,6 +1,11 @@
 import pyspark.sql.functions as F
 import logging
 
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    )
+
 def start():
   logging.info("Starting processing job.")
 
